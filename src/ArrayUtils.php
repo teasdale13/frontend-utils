74019aa3c0248ad4ca33bf37e57d1305b884f21e
Kevin Teasdale-Dubé <kevin_teasdale_dube@hotmail.com>
2020-07-03 00:10:13 -0400

    add comments

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -2,13 +2,21 @@
 
 namespace Frontend\Utils;
 
+use Collator;
+
 class ArrayUtils {
 
+    /**
+     * Takes an array and transform to a string.
+     *
+     * @param $array array we want to stringify
+     * @return array|string the string construct with the array values separated by a comma.
+     */
     public static function stringnify_array ( $array ) {
         if ( ! is_array( $array ) || empty( $array ) ) return $array;
 
         $temp_arr = [];
-        foreach ( $array as $item) {
+        foreach ( $array as $item ) {
             if ( is_array( $item ) ) {
                 foreach ( $item as $value ) {
                     $temp_arr[] = $value;
@@ -17,12 +25,17 @@ class ArrayUtils {
                 $temp_arr[] = $item;
             }
         }
-        return implode( ', ', $temp_arr);
+        return implode( ', ', $temp_arr );
     }
 
+    /**
+     * Sort an array with UTF-8 encoding
+     *
+     * @param $input_arr
+     * @return mixed
+     */
     public static function sort_fr( $input_arr ) {
-        $collator = new \Collator('fr_FR');
-        $collator->sort($input_arr, SORT_STRING );
+        ( new Collator( 'fr_FR' ) )->sort($input_arr, SORT_STRING );
         return $input_arr;
     }
 
--- src/AttributeUtils.php
+++ src/AttributeUtils.php
@@ -15,24 +15,40 @@ class AttributeUtils {
 
     /**
      * Remove the class attribute from an HTML tag
+     *
      * @param $input String HTML tags to remove class attribute.
      * @return string|string[]|null The input without the style attribute.
      */
     public static function remove_class( $input ) : string {
-        return preg_replace( '/\sclass="(.*)"/', '', $input );
+        return preg_replace('/\sclass="(.*)"/', '', $input );
     }
 
+    /**
+     * Add classes to <p>. But before we remove style attribute and also remove the classes that
+     * are already apply.
+     *
+     * @param $input string <p> tag(s) we want to add classes.
+     * @param array $classes classes to add to paragraph.
+     * @return string the <p> tag(s) with the added classes.
+     */
     public static function add_class_to_paragraph( $input, $classes = [] ) : string {
         if ( ! $input ) return '';
         $input = self::remove_class( self::remove_style( $input ) );
         if (  empty( $classes ) ) return $input;
 
-        $replace = '<p class="' . implode(' ', $classes) . '"$1';
-        return preg_replace('/<p([> ])/', $replace, $input);
+        $replace = '<p class="' . implode( ' ', $classes ) . '"$1';
+        return preg_replace('/<p([> ])/', $replace, $input );
     }
 
+    /**
+     * Remove the HTTP and HTTPS from the email. The ACF (Wordpress Plugin) link field add that
+     * to the input.
+     *
+     * @param $email string The link we want to sanitize (https://info@example.com)
+     * @return string the sanitized email. (info@example.com)
+     */
     private static function sanitize_for_email( $email ) : string {
-        return str_replace('https://', '', str_replace('http://', '', $email));
+        return str_replace('https://', '', str_replace('http://', '', $email ) );
     }
 
     /**
